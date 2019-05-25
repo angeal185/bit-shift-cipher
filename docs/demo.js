ddad9e524297e4403b1bfa04877aa01fecd4ec8f
ben <beneaves01@hotmail.com>
2019-05-25 14:24:55 +0800

    init

--- docs/demo.js
+++ docs/demo.js
@@ -1,4 +1,4 @@
-Curveconst conf = {
+const conf = {
   min: 0,
   max: 255,
   out: 'string',
@@ -11,7 +11,7 @@ Hash = '256',
 Curve = '521',
 cl = console.log,
 ce = console.error;
-
+/*
 let dtype = [
   'hex',
   'binary',
@@ -248,3 +248,86 @@ bsc.ecdsa.genP('521')
 }).catch(function(err){
   ce(err)
 })
+
+*/
+
+
+function completetest(){
+  let txt = $('#text').val(),
+  crv = $('#curve').val(),
+  hsh = $('#hash').val(),
+  dgs = $('#digest').val();
+  // enc/dec ecdsa test
+  bsc.ecdsa.gen(crv, function(err, ekey){
+    bsc.hmac.gen(hsh, dgs, function(err, hkey){
+     //cl(ekey)
+
+      bsc.encEcdsa(txt, ekey.private, hsh, dgs, function(err, res){
+        if(err){return ce(err)}
+        cl(res)
+
+        bsc.hmac.sign(hkey, res.ctext, hsh, dgs, function(err, sig){
+          if(err){return ce(err)}
+          res.ecdsa = ekey;
+          res.hmac = sig;
+
+          bsc.decEcdsa(res.ctext, res.key, res.sig, ekey.public, hsh, dgs, function(err, dec){
+            if(err){return ce(err)}
+
+            bsc.hmac.verify(hkey, res.ctext, res.hmac, hsh, dgs, function(err, isEqual){
+              if(err){return ce(err)}
+              cl(res)
+              $('#key').val(res.key)
+              $('#hkey').val(hkey)
+              $('#hsig').val(res.hmac)
+              $('#ekey').val(JSON.stringify(ekey))
+              $('#esig').val(res.sig)
+              $('#ciphertext').val(res.ctext)
+              $('#res').text(JSON.stringify(res,0,2))
+            })
+
+          });
+        });
+      })
+    })
+  })
+}
+
+let sigres = {
+  hkey: 'hmac key',
+  hsig: 'hmac signature',
+  ekey: 'ecdsa key',
+  esig: 'ecdsa signature'
+}
+
+$('body').append('<div class="container-fluid"><h1 class="text-center mt-4 mb-4">bit-shift-cipher</h1><div class="row main"></div></div>')
+
+$.each(['digest', 'hash', 'curve'], function(e,i){
+  $('.main').append('<div class="col-sm-4"><label>Curve</label><select class="form-control" id="'+ i +'"></select></div>')
+})
+
+$.each(['hex', 'base64', 'bytes', 'binary', 'uint8'], function(e,i){
+  $('#digest').append('<option value="'+ i +'">'+ i +'</option>')
+})
+
+$.each(['256', '384', '512'], function(e,i){
+  $('#hash').append('<option value="'+ i +'">SHA-'+ i +'</option>')
+})
+
+$.each(['256', '384', '521'], function(e,i){
+  $('#curve').append('<option value="'+ i +'">P-'+ i +'</option>')
+})
+
+$.each(['text', 'ciphertext', 'key'], function(e,i){
+  $('.main').append('<div class="col-sm-4 mt-4"><label>'+ i +'</label><textarea class="form-control" id="'+ i +'" rows="6"></textarea></div>')
+})
+
+$.each(sigres, function(e,i){
+  $('.main').append('<div class="col-sm-6 mt-4"><label>'+ i +'</label><input class="form-control" id="'+ e +'"></div>')
+})
+
+$('.main').append('<div class="col-sm-12 mt-4"><label>result</label><pre><code id="res"></code></pre></div>')
+
+$('#text').on('keyup', function(event) {
+  completetest()
+});
